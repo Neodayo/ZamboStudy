1e857001f79e8447f1f4fea2cf9643eec1eee2db
Neo <bonvon1232@gmail.com>
2024-12-14 17:21:36 +0800

    Initial commit

---
+++ signup.php
@@ -0,0 +1,43 @@
+<?php
+?>   
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/signup.css">
+    <title>Zambo Study - Sign Up</title>
+
+
+</head>
+
+<body>
+    <div class="signup-container">
+        <div class="title">
+            ZamboStudy
+        </div>
+
+        <label>Name</label>
+        <div class="input-group">
+            <input type="text" name="name" placeholder="Name" id="name"require>
+            
+        </div>
+
+        <label>Email</label>
+        <div class="input-group">
+            <input type="number" name="email" placeholder="Email" id="email" require>
+            
+        </div>
+
+        <label>Password</label>
+        <input type="text" placeholder="Password" class="password">
+
+        <label>What subject/s are you best in?</label>
+        <input type="text" placeholder="Subjects" class="full-width">
+
+        <button class="submit-button">Create account</button>
+    </div>
+</body>
+
+</html>
