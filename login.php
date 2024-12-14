3336238684676e623071ce425e48aa28a34d1bd0
Neo <bonvon1232@gmail.com>
2024-12-14 19:25:52 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,38 @@
-    <!DOCTYPE html>
+<?php
+// Start session
+session_start();
+
+// Include database connection
+include 'db_connect.php';
+
+$message = ""; // To display login error messages
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $Email = $_POST['Email'];
+    $Password = md5($_POST['Password']); // Password hashed with MD5  
+
+    // Check user in the database
+    $sql = "SELECT * FROM user WHERE Email = '$Email' AND Password = '$Password'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows == 1) {
+        // Fetch the user data
+        $user = $result->fetch_assoc();
+        $_SESSION['user_id'] = $user['user_id'];
+        $_SESSION['Name'] = $user['Name'];
+
+    
+        //just for debugging
+        echo "Session Debug: <br>";
+        echo "Id: " . $_SESSION['user_id'] . "<br>";
+        echo "First Name: " . $_SESSION['Name'] . "<br>";
+     
+        header("Location: homepage.html");
+    }
+}
+?>
+
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -13,13 +47,17 @@
     <div class="login-container">
         <div class="logo">
             ZAMBO<br>
-
+            <!-- <img src="images/ZamboStudyLogoClear.png" alt="ZamboStudy Logo" class="logo"> -->
         </div>
-        <input type="text" placeholder="Username">
-        <input type="password" placeholder="Password">
-        <button class="login-button">Log In</button>
-        <button class="signup-button">Sign up</button>
+
+        <form method="POST" action="">
+        <input type="text" name="Email" placeholder="Email" id="Email">
+        <input type="password" name="Password" placeholder="Password" id="Password">
+        <p>Don't have an account? <a href="signup.php   ">Signup here</a>.</p>
+        <button type="submit" class="login-button">Log In</button>
+        
+        
+        
     </div>
 </body>
-
 </html>
