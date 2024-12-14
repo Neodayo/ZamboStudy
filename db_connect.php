9c7814045f4033ff043d5c070bb7ee1740aa605a
Neo <bonvon1232@gmail.com>
2024-12-14 18:11:27 +0800

    signup attemp

--- db_connect.php
+++ db_connect.php
@@ -1,5 +1,12 @@
-<?php ?>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "zambostudy";
 
-this is a trait_exists
+$conn = new mysqli($servername, $username, $password, $database);
 
-tite
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
--- signup.php
+++ signup.php
@@ -1,5 +1,36 @@
 <?php
-?>   
+// Include database connection
+include 'C:\xampp\htdocs\HarvestHub\db_connect.php';
+
+$message = ""; // To display success or error messages
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $Name = $_POST['Name'];
+    $Email = $_POST['Email'];
+    $Password = md5($_POST['Password']); // Hash the password
+    $Bourse = $_POST['Course'];
+    $Barangay = $_POST['Barangay'];
+
+
+    // Check if the email already exists
+    $check_email = "SELECT * FROM user WHERE email = '$email'";
+    $result = $conn->query($check_email);
+    
+    if ($result->num_rows > 0) {
+        $message = "This email is already registered.";
+    } else {
+        // Insert new user into the database
+        $sql = "INSERT INTO user (name, email, password, course, barangay ) VALUES ('$Name', '$Email', '$Password', '$Course', '$Barangay')";
+        
+        if ($conn->query($sql) === TRUE) {
+            $message = "Registration successful! You can now log in.";
+        } else {
+            $message = "Error: " . $conn->error;
+        }
+    }
+}
+?> 
 <!DOCTYPE html>
 <html lang="en">
 
@@ -20,21 +51,21 @@
 
         <label>Name</label>
         <div class="input-group">
-            <input type="text" name="name" placeholder="tite " id="name"require>
+            <input type="text" name="Name" placeholder="Name " id="Name" require>
             
         </div>
 
         <label>Email</label>
         <div class="input-group">
-            <input type="number" name="email" placeholder="Email" id="email" require>
+            <input type="number" name="Email" placeholder="Email" id="Email" require>
             
         </div>
 
         <label>Password</label>
-        <input type="text" placeholder="Password" class="password">
+        <input type="text" placeholder="Password" id="Password" class="Password">
 
         <label>What subject/s are you best in?</label>
-        <input type="text" placeholder="Subjects" class="full-width">
+        <input type="text" name="Course" placeholder="Course" class="Course">
 
         <button class="submit-button">Create account</button>
     </div>
